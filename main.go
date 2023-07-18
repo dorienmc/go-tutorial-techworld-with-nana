ba15ee37047501e20b0cc3a15034d667bfbc5cb2
Dorien Lorijn - Meijer Cluwen <dorien.meijercluwen@nedap.com>
2023-07-18 15:26:19 +0200

    Arrays and slices

--- main.go
+++ main.go
@@ -11,6 +11,11 @@ func main(){
 	fmt.Printf("We have a total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 	
+	// Array vs. slice (dynamic array)
+	// array: var bookings [50]string //[50]string{}
+	// slice
+	var bookings []string
+	
 	var firstName string
 	var lastName string
 	var email string
@@ -29,7 +34,11 @@ func main(){
 	fmt.Scan(&userTickets)
 	
 	remainingTickets -= userTickets
+	// array: bookings[0] = firstName + " " + lastName
+	bookings = append(bookings, firstName + " " + lastName)
 	
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v.\n", firstName, lastName, userTickets, email)
 	fmt.Printf("There are %v tickets lef for %v.\n", remainingTickets, conferenceName)
+	
+	fmt.Printf("All bookings: %v\n", bookings)
 }
