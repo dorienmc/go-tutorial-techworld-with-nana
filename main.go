e22dd45fd51c75c98ad13a24fc3939ab4706dc43
Dorien Lorijn - Meijer Cluwen <dorien.meijercluwen@nedap.com>
2023-07-18 14:37:24 +0200

    variables and constants

--- main.go
+++ main.go
@@ -4,7 +4,10 @@ import "fmt"
 
 func main(){
 	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
 	
-	fmt.Println("Welcome to", conferenceName, "booking application")
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have a total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 }
