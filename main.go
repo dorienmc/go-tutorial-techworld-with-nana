8d49c25d051346b9a349a7d19af502f8b9b54d22
Dorien Lorijn - Meijer Cluwen <dorien.meijercluwen@nedap.com>
2023-07-18 14:55:15 +0200

    get user input

--- main.go
+++ main.go
@@ -11,11 +11,22 @@ func main(){
 	fmt.Printf("We have a total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 	
-	var userName string
+	var firstName string
+	var lastName string
+	var email string
 	var userTickets int
 	// ask user for name
+	fmt.Print("Enter your first name: ")
+	fmt.Scan(&firstName)
 	
-	userName = "Tom"
-	userTickets = 2
-	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
+	fmt.Print("Enter your last name: ")
+	fmt.Scan(&lastName)
+	
+	fmt.Print("Enter your email: ")
+	fmt.Scan(&email)
+	// ask for amount of tickets
+	fmt.Print("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+	
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v.\n", firstName, lastName, userTickets, email)
 }
